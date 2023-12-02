bf04f47d7a38c0d0d522c44e69133cd0aabb3651
HuzaifaRizwan1231 <huzaifa.rizwan1231@gmail.com>
2023-12-03 01:36:25 +0500

    Modified DashBoard For User

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -252,6 +252,27 @@ const Dashboard = () => {
                       </span>
                     </a>
                   </li>
+                  <li>
+                    <a
+                      href="#"
+                      class=" cur flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
+                    >
+                      <svg
+                        class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                      >
+                        <path d="M5 5V.13a2.96 2.96 0 0 0-1.293.749L.879 3.707A2.96 2.96 0 0 0 .13 5H5Z" />
+                        <path d="M6.737 11.061a2.961 2.961 0 0 1 .81-1.515l6.117-6.116A4.839 4.839 0 0 1 16 2.141V2a1.97 1.97 0 0 0-1.933-2H7v5a2 2 0 0 1-2 2H0v11a1.969 1.969 0 0 0 1.933 2h12.134A1.97 1.97 0 0 0 16 18v-3.093l-1.546 1.546c-.413.413-.94.695-1.513.81l-3.4.679a2.947 2.947 0 0 1-1.85-.227 2.96 2.96 0 0 1-1.635-3.257l.681-3.397Z" />
+                        <path d="M8.961 16a.93.93 0 0 0 .189-.019l3.4-.679a.961.961 0 0 0 .49-.263l6.118-6.117a2.884 2.884 0 0 0-4.079-4.078l-6.117 6.117a.96.96 0 0 0-.263.491l-.679 3.4A.961.961 0 0 0 8.961 16Zm7.477-9.8a.958.958 0 0 1 .68-.281.961.961 0 0 1 .682 1.644l-.315.315-1.36-1.36.313-.318Zm-5.911 5.911 4.236-4.236 1.359 1.359-4.236 4.237-1.7.339.341-1.699Z" />
+                      </svg>
+                      <span class="flex-1 ms-3">
+                        {user !== null ? <p>{user.email}</p> : <p>No User</p>}
+                      </span>
+                    </a>
+                  </li>
                 </ul>
               </div>
             </aside>
@@ -262,7 +283,6 @@ const Dashboard = () => {
                 <span>loading..</span>
               ) : (
                 <div>
-                  {user !== null ? <p>{user.email}</p> : <p>No User</p>}
                   {user?.email === "saadamir318@gmail.com" ? (
                     <div>
                       <h2>Bill Entries</h2>
@@ -304,9 +324,9 @@ const Dashboard = () => {
                     <ul></ul>
                   )}
                   {showTime ? (
-                    <div class="Dialog">
+                    <div class="Dialog mt-4">
                       <table class="w-full text-sm text-left rtl:text-right dark:text-gray-400">
-                        <thead class="text-xs  uppercase">
+                        <thead class="text-xs uppercase">
                           <tr>
                             <th scope="col" class="px-6 py-3">
                               Load Shedding Duration
@@ -320,22 +340,40 @@ const Dashboard = () => {
                           </tr>
                         </thead>
                         <tbody>
-                        {loadSheddingDetails.map((detail, index) => (
-                          <tr class="odd:bg-white odd:dark:bg-gray-900 border-b dark:border-gray-700" key={index}>
-                            <td scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">{detail.duration}</td>
-                            <td class="px-6 py-4">{detail.area}</td>
-                            <td class="px-6 py-4">{detail.date}</td>
-                          </tr>
-                        ))}
+                          {loadSheddingDetails.map((detail, index) => (
+                            <tr
+                              class="odd:bg-white odd:dark:bg-gray-900  border-b dark:border-gray-700"
+                              key={index}
+                            >
+                              <td
+                                scope="row"
+                                class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                              >
+                                {detail.duration}
+                              </td>
+                              <td class="px-6 py-4">{detail.area}</td>
+                              <td class="px-6 py-4">{detail.date}</td>
+                            </tr>
+                          ))}
+                          <thead>
+                            <tr>
+                              <th class="px-6 py-3"></th>
+                              <th class="px-6 py-3"></th>
+                              <th class="px-6 py-3"></th>
+                            </tr>
+                          </thead>
                         </tbody>
                       </table>
-                      <button className="button"
-                      onClick={() => {
-                        setShowTime(!showTime);
-                      }}
-                    >
-                      close
-                    </button>
+                      <div className="text-center">
+                        <button
+                          className="CustomButton"
+                          onClick={() => {
+                            setShowTime(!showTime);
+                          }}
+                        >
+                          close
+                        </button>
+                      </div>
                     </div>
                   ) : null}
                   {payBills ? (
