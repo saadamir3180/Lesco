7d37c1d56ba0d9713ab19c0eba5af278e62fdf7d
HuzaifaRizwan1231 <huzaifa.rizwan1231@gmail.com>
2023-12-03 23:51:08 +0500

    Fixed Table of Admin

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -105,7 +105,8 @@ const Dashboard = () => {
     }
   }
 
-
+  let userId = 1;
+ 
   const [entries, setEntries] = useState(billEntries);
 
   const handleChangeStatus = (id) => {
@@ -213,18 +214,18 @@ const Dashboard = () => {
                   <div className="col-md-3">
                     <aside
                       id="sidebar-multi-level-sidebar"
-                      class=" z-40  h-full transition-transform -translate-x-0 "
+                      className=" z-40  h-full transition-transform -translate-x-0 "
                       aria-label="Sidebar"
                     >
-                      <div class=" DashBoard h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
-                        <ul class="space-y-2 font-medium">
+                      <div className=" DashBoard h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
+                        <ul className="space-y-2 font-medium">
                           <li>
                             <a
                               href="#"
-                              class=" cur flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
+                              className=" cur flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
                             >
                               <svg
-                                class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                                className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
                                 aria-hidden="true"
                                 xmlns="http://www.w3.org/2000/svg"
                                 fill="currentColor"
@@ -234,7 +235,7 @@ const Dashboard = () => {
                                 <path d="M6.737 11.061a2.961 2.961 0 0 1 .81-1.515l6.117-6.116A4.839 4.839 0 0 1 16 2.141V2a1.97 1.97 0 0 0-1.933-2H7v5a2 2 0 0 1-2 2H0v11a1.969 1.969 0 0 0 1.933 2h12.134A1.97 1.97 0 0 0 16 18v-3.093l-1.546 1.546c-.413.413-.94.695-1.513.81l-3.4.679a2.947 2.947 0 0 1-1.85-.227 2.96 2.96 0 0 1-1.635-3.257l.681-3.397Z" />
                                 <path d="M8.961 16a.93.93 0 0 0 .189-.019l3.4-.679a.961.961 0 0 0 .49-.263l6.118-6.117a2.884 2.884 0 0 0-4.079-4.078l-6.117 6.117a.96.96 0 0 0-.263.491l-.679 3.4A.961.961 0 0 0 8.961 16Zm7.477-9.8a.958.958 0 0 1 .68-.281.961.961 0 0 1 .682 1.644l-.315.315-1.36-1.36.313-.318Zm-5.911 5.911 4.236-4.236 1.359 1.359-4.236 4.237-1.7.339.341-1.699Z" />
                               </svg>
-                              <span class="flex-1 ms-3 whitespace-nowrap">
+                              <span className="flex-1 ms-3 whitespace-nowrap">
                                 View Bills
                               </span>
                             </a>
@@ -242,7 +243,7 @@ const Dashboard = () => {
                           <li>
                             <a
                               href="#"
-                              class="cur flex p-2 mb-3 bottom-0 fixed text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
+                              className="cur flex p-2 mb-3 bottom-0 fixed text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
                             >
                               {user !== null ? (
                                 <span>{user.email}</span>
@@ -258,28 +259,31 @@ const Dashboard = () => {
                   <div className="col-md-9">
                     <div className="Dialog">
                       <h1 className="pageHeading my-4"><b>Bill Entries</b></h1>
-                      <table class="text-sm text-left rtl:text-right dark:text-gray-400">
-                        <thead class="text-xs uppercase">
+                      <table className="text-sm text-left rtl:text-right dark:text-gray-400">
+                        <thead className="text-xs uppercase">
                           <tr>
-                            <th scope="col" class="px-6 py-3">
+                            <th scope="col" className="px-3 py-3">
+                              Sr. No.
+                            </th>
+                            <th scope="col" className="px-3 py-3">
                               User CNIC
                             </th>
-                            <th scope="col" class="px-6 py-3">
+                            <th scope="col" className="px-3 py-3">
                               User Email
                             </th>
-                            <th scope="col" class="px-6 py-3">
+                            <th scope="col" className="px-3 py-3">
                               Bill Amount
                             </th>
-                            <th scope="col" class="px-6 py-3">
+                            <th scope="col" className="px-3 py-3">
                               Bill ID
                             </th>
-                            <th scope="col" class="px-6 py-3">
+                            <th scope="col" className="px-3 py-3">
                               Due Date
                             </th>
-                            <th scope="col" class="px-6 py-3">
+                            <th scope="col" className="px-3 py-3">
                               Status
                             </th>
-                            <th scope="col" class="px-6 py-3">
+                            <th scope="col" className="px-3 py-3">
                               Action
                             </th>
                           </tr>
@@ -287,21 +291,27 @@ const Dashboard = () => {
                         <tbody>
                           {entries.map((entry) => (
                             <tr
-                              class="odd:bg-white odd:dark:bg-gray-900  border-b dark:border-gray-700"
+                              className="odd:bg-white odd:dark:bg-gray-900  border-b dark:border-gray-700"
                               key={entry.id}
                             >
+                              <th
+                                scope="row"
+                                className="px-3 py-4"
+                              >
+                               {userId++}
+                              </th>
                               <td
                                 scope="row"
-                                class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                                className="px-3 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
                               >
                                 {entry.cnic}
                               </td>
-                              <td class="px-6 py-4">{entry.email || "N/A"}</td>
-                              <td class="px-6 py-4">{entry.amount}</td>
-                              <td class="px-6 py-4">{entry.id}</td>
-                              <td class="px-6 py-4">{entry.dueDate}</td>
-                              <td class="px-6 py-4 uppercase"  style={getStatusColor(entry.status)} >{entry.status}</td>
-                              <td class="px-6 py-4">
+                              <td className="px-3 py-4">{entry.email || "N/A"}</td>
+                              <td className="px-3 py-4">{entry.amount}</td>
+                              <td className="px-3 py-4">{entry.id}</td>
+                              <td className="px-3 py-4">{entry.dueDate}</td>
+                              <td className="px-3 py-4 uppercase"  style={getStatusColor(entry.status)} >{entry.status}</td>
+                              <td className="px-3 py-4">
                                 <button
                                   onClick={() => handleChangeStatus(entry.id)}
                                 >
@@ -310,13 +320,13 @@ const Dashboard = () => {
                               </td>
                             </tr>
                           ))}
-                          <thead>
+                          
                           <tr>
-                            <th class="px-6 py-3"></th>
-                            <th class="px-6 py-3"></th>
-                            <th class="px-6 py-3"></th>
+                            <th className="px-6 py-3"></th>
+                            <th className="px-6 py-3"></th>
+                            <th className="px-6 py-3"></th>
                           </tr>
-                        </thead>
+                        
                         </tbody>
                       </table>
                     </div>
@@ -327,21 +337,21 @@ const Dashboard = () => {
                 <div className="col-md-3">
                   <aside
                     id="sidebar-multi-level-sidebar"
-                    class=" z-40  h-full transition-transform -translate-x-0 "
+                    className=" z-40  h-full transition-transform -translate-x-0 "
                     aria-label="Sidebar"
                   >
-                    <div class=" DashBoard h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
-                      <ul class="space-y-2 font-medium">
+                    <div className=" DashBoard h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
+                      <ul className="space-y-2 font-medium">
                         <li>
                           <a
                             onClick={() => {
                               setShowTime(true);
                               setPayBills(false);
                             }}
-                            class="cur flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
+                            className="cur flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
                           >
                             <svg
-                              class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                              className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
                               aria-hidden="true"
                               xmlns="http://www.w3.org/2000/svg"
                               fill="none"
@@ -355,7 +365,7 @@ const Dashboard = () => {
                                 d="M1 8h11m0 0L8 4m4 4-4 4m4-11h3a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-3"
                               />
                             </svg>
-                            <span class="flex-1 ms-3">
+                            <span className="flex-1 ms-3">
                               Load Shedding Schedule
                             </span>
                           </a>
@@ -367,10 +377,10 @@ const Dashboard = () => {
                               setShowTime(false);
                             }}
                             href="#"
-                            class=" cur flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
+                            className=" cur flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
                           >
                             <svg
-                              class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                              className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
                               aria-hidden="true"
                               xmlns="http://www.w3.org/2000/svg"
                               fill="currentColor"
@@ -380,7 +390,7 @@ const Dashboard = () => {
                               <path d="M6.737 11.061a2.961 2.961 0 0 1 .81-1.515l6.117-6.116A4.839 4.839 0 0 1 16 2.141V2a1.97 1.97 0 0 0-1.933-2H7v5a2 2 0 0 1-2 2H0v11a1.969 1.969 0 0 0 1.933 2h12.134A1.97 1.97 0 0 0 16 18v-3.093l-1.546 1.546c-.413.413-.94.695-1.513.81l-3.4.679a2.947 2.947 0 0 1-1.85-.227 2.96 2.96 0 0 1-1.635-3.257l.681-3.397Z" />
                               <path d="M8.961 16a.93.93 0 0 0 .189-.019l3.4-.679a.961.961 0 0 0 .49-.263l6.118-6.117a2.884 2.884 0 0 0-4.079-4.078l-6.117 6.117a.96.96 0 0 0-.263.491l-.679 3.4A.961.961 0 0 0 8.961 16Zm7.477-9.8a.958.958 0 0 1 .68-.281.961.961 0 0 1 .682 1.644l-.315.315-1.36-1.36.313-.318Zm-5.911 5.911 4.236-4.236 1.359 1.359-4.236 4.237-1.7.339.341-1.699Z" />
                             </svg>
-                            <span class="flex-1 ms-3 whitespace-nowrap">
+                            <span className="flex-1 ms-3 whitespace-nowrap">
                               Pay Bill
                             </span>
                           </a>
@@ -388,7 +398,7 @@ const Dashboard = () => {
                         <li>
                           <a
                             href="#"
-                            class="cur flex p-2 mb-3 bottom-0 fixed text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
+                            className="cur flex p-2 mb-3 bottom-0 fixed text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
                           >
                             {user !== null ? (
                               <span>{user.email}</span>
@@ -404,17 +414,17 @@ const Dashboard = () => {
               )}
               {showTime ? (
                 <div className="col-md-9">
-                  <div class="Dialog mt-4">
-                    <table class="w-full text-sm text-left rtl:text-right dark:text-gray-400">
-                      <thead class="text-xs uppercase">
+                  <div className="Dialog mt-4">
+                    <table className="w-full text-sm text-left rtl:text-right dark:text-gray-400">
+                      <thead className="text-xs uppercase">
                         <tr>
-                          <th scope="col" class="px-6 py-3">
+                          <th scope="col" className="px-6 py-3">
                             Load Shedding Duration
                           </th>
-                          <th scope="col" class="px-6 py-3">
+                          <th scope="col" className="px-6 py-3">
                             Area
                           </th>
-                          <th scope="col" class="px-6 py-3">
+                          <th scope="col" className="px-6 py-3">
                             Date
                           </th>
                         </tr>
@@ -422,26 +432,26 @@ const Dashboard = () => {
                       <tbody>
                         {loadSheddingDetails.map((detail, index) => (
                           <tr
-                            class="odd:bg-white odd:dark:bg-gray-900  border-b dark:border-gray-700"
+                            className="odd:bg-white odd:dark:bg-gray-900  border-b dark:border-gray-700"
                             key={index}
                           >
                             <td
                               scope="row"
-                              class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                              className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
                             >
                               {detail.duration}
                             </td>
-                            <td class="px-6 py-4">{detail.area}</td>
-                            <td class="px-6 py-4">{detail.date}</td>
+                            <td className="px-6 py-4">{detail.area}</td>
+                            <td className="px-6 py-4">{detail.date}</td>
                           </tr>
                         ))}
-                        <thead>
+                       
                           <tr>
-                            <th class="px-6 py-3"></th>
-                            <th class="px-6 py-3"></th>
-                            <th class="px-6 py-3"></th>
+                            <th className="px-6 py-3"></th>
+                            <th className="px-6 py-3"></th>
+                            <th className="px-6 py-3"></th>
                           </tr>
-                        </thead>
+                       
                       </tbody>
                     </table>
                     <div className="text-center">
